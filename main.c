6da7296389bc5a6abf29a2db262f291e441cf729
hiteshK03 <1603coderz@gmail.com>
2021-04-04 22:23:25 +0530

    small alphabets added

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <at89c5131.h>
-#include "lcd.h"		//Header file with LCD interfacing functions
-#include "serial.c"	//C file with UART interfacing functions
+#include "lcd_p.h"		//Header file with LCD interfacing functions
+#include "serial_p.c"	//C file with UART interfacing functions
 #include "tone.c"
 
 sbit LED=P1^7;
 
-//Test function definitions
+//function definitions
 
 /************************************************
-   lcd_test():
-	LCD test function that gets called when
-	'1' is received from UART
+   lcd_start():
+	Prints "Input please" on LCD till a character
+	is pressed.
+   lcd_inv():
+    Print "Invalid input" on LCD for 2 secs
 ************************************************/
 void lcd_start(void)
 {
@@ -81,48 +83,42 @@ void main(void)
 						  dash();
 						  break;
 
-				case '4': transmit_string("User Pressed : 4\r\n");
-						  dot_s();
+				case '4': dot_s();
 						  dot_s();
 						  dot_s();
 						  dot_s();
 						  dash();
 						  break;
 
-				case '5': transmit_string("User Pressed : 5\r\n");
-						  dot_s();
-						  dot_s();
-						  dot_s();
-						  dot_s();
-						  dot();
-						  break;
-
-				case '6': transmit_string("User Pressed : 6\r\n");
-						  dash_s();
+				case '5': dot_s();
 						  dot_s();
 						  dot_s();
 						  dot_s();
 						  dot();
 						  break;
 
-				case '7': transmit_string("User Pressed : 7\r\n");
-						  dash_s();
+				case '6': dash_s();
+						  dot_s();
+						  dot_s();
+						  dot_s();
+						  dot();
+						  break;
+
+				case '7': dash_s();
 						  dash_s();
 						  dot_s();
 						  dot_s();
 						  dot();
 						  break;
 
-				case '8': transmit_string("User Pressed : 8\r\n");
-						  dash_s();
+				case '8': dash_s();
 						  dash_s();
 						  dash_s();
 						  dot_s();
 						  dot();
 						  break;
 
-				case '9': transmit_string("User Pressed : 9\r\n");
-						  dash_s();
+				case '9': dash_s();
 						  dash_s();
 						  dash_s();
 						  dash_s();
@@ -137,18 +133,21 @@ void main(void)
 						  dash();
 						  break;
 
+				case 'a':
 				case 'A': transmit_string("User Pressed : A\r\n");
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'b':
 				case 'B': transmit_string("User Pressed : B\r\n");
 						  dash_s();
 						  dot_s();
 						  dot_s();
 						  dot();
 						  break;
-
+				
+				case 'c':
 				case 'C': transmit_string("User Pressed : C\r\n");
 						  dash_s();
 						  dot_s();
@@ -156,119 +155,142 @@ void main(void)
 						  dot();
 						  break;
 
+				case 'd':
 				case 'D': transmit_string("User Pressed : D\r\n");
 						  dash_s();
 						  dot_s();
 						  dot();
 						  break;
 
+				case 'e':
 				case 'E': dot();
 						  break;
 
+				case 'f':
 				case 'F': dot_s();
 						  dot_s();
 						  dash_s();
 						  dot();
 						  break;
 
+				case 'g':
 				case 'G': dash_s();
 						  dash_s();
 						  dot();
 						  break;
 
+				case 'h':
 				case 'H': dot_s();
 						  dot_s();
 						  dot_s();
 						  dot();
 						  break;
 
+				case 'i':
 				case 'I': dot_s();
 						  dot();
 						  break;
 
+				case 'j':
 				case 'J': dot_s();
 						  dash_s();
 						  dash_s();
 						  dash();
 						  break;
 
+				case 'k':
 				case 'K': dash_s();
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'l':
 				case 'L': dot_s();
 						  dash_s();
 						  dot_s();
 						  dot();
 						  break;
 
+				case 'm':
 				case 'M': dash_s();
 						  dash();
 						  break;
 
+				case 'n':
 				case 'N': dash_s();
 						  dot();
 						  break;
 
+				case 'o':
 				case 'O': dash_s();
 						  dash_s();
 						  dash();
 						  break;
 
+				case 'p':
 				case 'P': dot_s();
 						  dash_s();
 						  dash_s();
 						  dot();
 						  break;
 
+				case 'q':
 				case 'Q': dash_s();
 						  dash_s();
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'r':
 				case 'R': dot_s();
 						  dash_s();
 						  dot();
 						  break;
 
+				case 's':
 				case 'S': dot_s();
 						  dot_s();
 						  dot();
 						  break;
 
+				case 't':
 				case 'T': dash();
 						  break;
 
+				case 'u':
 				case 'U': dot_s();
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'v':
 				case 'V': dot_s();
 						  dot_s();
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'w':
 				case 'W': dot_s();
 						  dash_s();
 						  dash();
 						  break;
 
+				case 'x':
 				case 'X': dash_s();
 						  dot_s();
 						  dot_s();
 						  dash();
 						  break;
 
+				case 'y':
 				case 'Y': dash_s();
 						  dot_s();
 						  dash_s();
 						  dash();
 						  break;
 
+				case 'z':
 				case 'Z': dash_s();
 						  dash_s();
 						  dot_s();
